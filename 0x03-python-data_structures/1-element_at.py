cd123e8c33884b9af0569b8952d7037444cfe009
MinightDev <onlyayoub565@gmail.com>
2023-09-10 13:15:13 -0700

    👌

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def element_at(my_list, i):
-    if i < 0 or i >= len(my_list):
-        raise IndexError("Index out of bounds")
-    return my_list[i]
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return (None)
+    return my_list[idx]
