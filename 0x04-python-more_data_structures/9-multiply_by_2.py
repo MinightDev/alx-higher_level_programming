99f5b95dbbbc9726644246d2ce96df4675a0d2d0
MinightDev <onlyayoub565@gmail.com>
2023-09-13 06:14:50 -0700

    ok

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary:
-        del a_dictionary[key]
-    return a_dictionary
+def multiply_by_2(a_dictionary):
+    new_dictionary = a_dictionary.copy()
+    for key, value in list(new_dictionary.items()):
+        new_dictionary[key] = value * 2
+    return new_dictionary
