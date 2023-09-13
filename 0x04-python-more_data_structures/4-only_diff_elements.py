b92d72cbbba17141eea8afbc8b0448acbc60bcbe
MinightDev <onlyayoub565@gmail.com>
2023-09-13 06:08:12 -0700

    ok

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    common_set = set_1.intersection(set_2)
-    return common_set
+def only_diff_elements(set_1, set_2):
+    return list(set(set_1) ^ set(set_2))
