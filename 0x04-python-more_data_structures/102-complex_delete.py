75ba4c05017ee6b5bf2eb9b4c71bbeabc51b6c97
MinightDev <onlyayoub565@gmail.com>
2023-09-14 15:37:02 -0700

    ok

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    new_dict = dict(a_dictionary)
+    for key, val in a_dictionary.items():
+        if val == value:
+            del new_dict[key]
+    return (new_dict)
