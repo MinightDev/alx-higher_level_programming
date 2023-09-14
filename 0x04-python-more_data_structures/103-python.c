dea47dd3a4d69afd5b706eccede91020d2b070fc
MinightDev <onlyayoub565@gmail.com>
2023-09-14 15:49:38 -0700

    ok

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,6 +1,37 @@
 #include <Python.h>
 
 /**
+ * print_python_bytes - Prints basic info about Python bytes objects.
+ * @p: A PyObject byte object.
+ */
+void print_python_bytes(PyObject *p)
+{
+	Py_ssize_t size, i, limit;
+	char *string;
+
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+	size = PyBytes_Size(p);
+	string = PyBytes_AsString(p);
+
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", string);
+
+	limit = (size > 10) ? 10 : size;
+
+	printf("  first %ld bytes:", limit);
+	for (i = 0; i < limit; i++)
+		printf(" %02hhx", string[i]);
+
+	printf("\n");
+}
+
+/**
  * print_python_list - Prints basic info about Python lists.
  * @p: A PyObject list object.
  */
@@ -13,7 +44,6 @@ void print_python_list(PyObject *p)
 
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
-
 	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
 	for (i = 0; i < size; i++)
@@ -25,31 +55,3 @@ void print_python_list(PyObject *p)
 			print_python_bytes(item);
 	}
 }
-
-/**
- * print_python_bytes - Prints basic info about Python byte objects.
- * @p: A PyObject byte object.
- */
-void print_python_bytes(PyObject *p)
-{
-	Py_ssize_t size, i;
-
-	size = PyBytes_Size(p);
-
-	printf("[.] bytes object info\n");
-	printf("  size: %ld\n", size);
-	printf("  trying string: %s\n", PyBytes_AsString(p));
-
-	if (size > 10)
-		size = 10;
-
-	printf("  first %ld bytes: ", size);
-	for (i = 0; i < size; i++)
-	{
-		printf("%02hhx", PyBytes_AsString(p)[i]);
-		if (i < size - 1)
-			printf(" ");
-	}
-	printf("\n");
-}
-
