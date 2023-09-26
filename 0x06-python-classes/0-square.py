6c0fde63fecc6fb3b28fc35a80e345f1acb98742
MinightDev <onlyayoub565@gmail.com>
2023-09-26 12:58:26 -0700

    Classes n objects

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module defines an empty class Square."""
+
+
+class Square:
+    """Empty class Square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """Square class with a private instance attribute: size."""
+    
+    def __init__(self, size):
+        """Initialize the Square instance."""
+        self.__size = size
