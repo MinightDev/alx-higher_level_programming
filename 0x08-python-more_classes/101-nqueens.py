8c87a8ffdedab51e194bda79b4adc31f55bfd00f
MinightDev <onlyayoub565@gmail.com>
2023-10-02 08:14:55 -0700

    More classes

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+nqueens backtracking program to print the coordinates of n queens
+on an nxn grid such that they are all in non-attacking positions
+"""
+
+
 import sys
 
 def is_safe(board, row, col):
