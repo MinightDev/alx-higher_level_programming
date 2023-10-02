d95cf7ddf3c50298432e16930f9de0e64332f2da
MinightDev <onlyayoub565@gmail.com>
2023-10-02 08:05:10 -0700

    More classes

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,31 +1,56 @@
 #!/usr/bin/python3
+"""
+Solves the N queens puzzle using backtracking.
+"""
+
 import sys
 
-def is_safe(board, row, col):
-    """
-    Check if it's safe to place a queen at a given position on the board.
-    """
-    for i in range(col):
-        if board[i] == row or \
-           board[i] - i == row - col or \
-           board[i] + i == row + col:
-            return False
-    return True
+def nqueens(N):
+    def is_safe(board, row, col):
+        """Check if it's safe to place a queen at board[row][col]."""
+        # Check the left side of the row
+        for i in range(col):
+            if board[row][i] == 1:
+                return False
 
-def solve_nqueens(n):
-    """
-    Solve the N Queens problem using backtracking.
-    """
-    def backtrack(board, col):
-        if col == n:
-            solutions.append(board[:])
+        # Check upper left diagonal
+        for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+            if board[i][j] == 1:
+                return False
+
+        # Check lower left diagonal
+        for i, j in zip(range(row, N), range(col, -1, -1)):
+            if board[i][j] == 1:
+                return False
+
+        return True
+
+    def solve_nqueens_util(board, col):
+        """Recursively solve the N Queens puzzle."""
+        if col >= N:
+            solutions.append([row[:] for row in board])
             return
-        for row in range(n):
-            if is_safe(board, row, col):
-                board[col] = row
-                backtrack(board, col + 1)
 
+        for i in range(N):
+            if is_safe(board, i, col):
+                board[i][col] = 1
+                solve_nqueens_util(board, col + 1)
+                board[i][col] = 0
+
+    if not N.isdigit():
+        print("N must be a number")
+        return
+
+    N = int(N)
+
+    if N < 4:
+        print("N must be at least 4")
+        return
+
+    board = [[0 for _ in range(N)] for _ in range(N)]
     solutions = []
-    board = [-1] * n
-    backtrack(board, 0)
-    return solutions
+
+    solve_nqueens_util(board, 0)
+
+    for solution in solutions:
+        print(solution)
