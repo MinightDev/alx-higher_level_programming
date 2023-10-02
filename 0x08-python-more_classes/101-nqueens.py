d2dbe3c7b637281f5304250c2240b823c7d97a96
MinightDev <onlyayoub565@gmail.com>
2023-10-02 08:19:51 -0700

    More classes

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,42 +1,40 @@
-#!/usr/bin/python3
+#!/usr/bin/python3i
 """
-N-Queens backtracking program to print the coordinates of N queens
-on an NxN grid such that they are all in non-attacking positions.
+This program uses backtracking to find and print the coordinates of N queens on an NxN grid,
+ensuring that they are all in non-attacking positions.
 """
+
 
 import sys
 
-def nqueens(N):
-    def is_safe(board, row, col):
-        """
-        Check if it's safe to place a queen at a given position on the board.
-        """
-        for i in range(col):
-            if board[i] == row or \
-               board[i] - i == row - col or \
-               board[i] + i == row + col:
-                return False
-        return True
+def is_safe(board, row, col):
+    """
+    Check if it's safe to place a queen at a given position on the board.
+    """
+    for i in range(col):
+        if board[i] == row or \
+           board[i] - i == row - col or \
+           board[i] + i == row + col:
+            return False
+    return True
 
-    def solve(board, col):
-        if col == N:
+def solve_nqueens(n):
+    """
+    Solve the N Queens problem using backtracking.
+    """
+    def backtrack(board, col):
+        if col == n:
             solutions.append(board[:])
             return
-        for row in range(N):
+        for row in range(n):
             if is_safe(board, row, col):
                 board[col] = row
-                solve(board, col + 1)
-
-    if not isinstance(N, int) or N < 4:
-        print("N must be a number and at least 4")
-        sys.exit(1)
+                backtrack(board, col + 1)
 
     solutions = []
-    board = [-1] * N
-    solve(board, 0)
-
-    for solution in solutions:
-        print([[i, solution[i]] for i in range(N)])
+    board = [-1] * n
+    backtrack(board, 0)
+    return solutions
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -45,7 +43,14 @@ if __name__ == "__main__":
 
     try:
         n = int(sys.argv[1])
-        nqueens(n)
+        if n < 4:
+            print("N must be at least 4")
+            sys.exit(1)
     except ValueError:
         print("N must be a number")
         sys.exit(1)
+
+    solutions = solve_nqueens(n)
+    for solution in solutions:
+        print([[i, solution[i]] for i in range(n)])
+
