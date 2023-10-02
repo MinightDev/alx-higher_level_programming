48e93d69a1fcbfc46eca944b8a83e4ba6ce51388
MinightDev <onlyayoub565@gmail.com>
2023-10-02 08:01:28 -0700

    More classes

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+import sys
+
+def is_safe(board, row, col):
+    """
+    Check if it's safe to place a queen at a given position on the board.
+    """
+    for i in range(col):
+        if board[i] == row or \
+           board[i] - i == row - col or \
+           board[i] + i == row + col:
+            return False
+    return True
+
+def solve_nqueens(n):
+    """
+    Solve the N Queens problem using backtracking.
+    """
+    def backtrack(board, col):
+        if col == n:
+            solutions.append(board[:])
+            return
+        for row in range(n):
+            if is_safe(board, row, col):
+                board[col] = row
+                backtrack(board, col + 1)
+
+    solutions = []
+    board = [-1] * n
+    backtrack(board, 0)
+    return solutions
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+        if n < 4:
+            print("N must be at least 4")
+            sys.exit(1)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    solutions = solve_nqueens(n)
+    for solution in solutions:
+        print([[i, solution[i]] for i in range(n)])
