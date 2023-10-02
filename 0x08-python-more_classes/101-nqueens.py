54fec1177de7962eac5a3a22ab17921ac8b4d5b5
MinightDev <onlyayoub565@gmail.com>
2023-10-02 08:04:22 -0700

    More classes

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -29,21 +29,3 @@ def solve_nqueens(n):
     board = [-1] * n
     backtrack(board, 0)
     return solutions
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        n = int(sys.argv[1])
-        if n < 4:
-            print("N must be at least 4")
-            sys.exit(1)
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-
-    solutions = solve_nqueens(n)
-    for solution in solutions:
-        print([[i, solution[i]] for i in range(n)])
