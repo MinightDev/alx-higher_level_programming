1f46bfbc8ab4b8e9b06c709216ac14d323b3371d
MinightDev <onlyayoub565@gmail.com>
2023-11-14 06:26:26 -0800

    ok

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,5 @@
--- CREATE A DATABASE, AND CHECK IF IT DOESNT EXIST
+-- creates the database hbtn_0c_0 in your MySQL server.
+-- If the database hbtn_0c_0 already exists, your script should not fail
+-- You are not allowed to use the SELECT or SHOW statements
 
-CREATE DATABSE IF NOT EXIST hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
