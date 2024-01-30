deb18170e0acebc3d79f2963786f016282ffabf4
Ayoub <onlyayoub565@gmail.com>
2024-01-30 08:08:31 -0800

    JS Web Scraping 🌐

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,45 +1,12 @@
 #!/usr/bin/node
-
 const request = require('request');
-const starWarsUri = process.argv[2];
-let count = 0;
-
-if (!starWarsUri) {
-  console.error('Usage: ./script.js <Star Wars API URL>');
-  process.exit(1);
-}
-
-request(starWarsUri, function (error, response, body) {
-  if (error) {
-    console.error('Error:', error);
-    process.exit(1);
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Unexpected status code:', response.statusCode);
-    process.exit(1);
-  }
-
-  try {
-    body = JSON.parse(body).results;
-
-    for (let i = 0; i < body.length; ++i) {
-      const characters = body[i].characters;
-
-      for (let j = 0; j < characters.length; ++j) {
-        const character = characters[j];
-        const characterId = character.split('/')[5];
-
-        if (characterId === '18') {
-          count += 1;
-        }
-      }
-    }
-
-    console.log(count);
-  } catch (parseError) {
-    console.error('Error parsing JSON:', parseError);
-    process.exit(1);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
-
