539ab7c3a6f2471959ed276137750840be3318e4
Ayoub <onlyayoub565@gmail.com>
2024-01-30 08:06:08 -0800

    JS Web Scraping 🌐

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,45 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
 
-function getDataFrom(url) {
-  return new Promise(function (resolve, reject) {
-    request(url, function (error, _res, body) {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(body);
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
       }
-    });
+    }
   });
 }
-
-function errorHandler(error) {
-  console.log(error);
-}
-
-function printMovieCharacters(movieId) {
-  const movieUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-
-  getDataFrom(movieUrl)
-    .then(JSON.parse, errorHandler)
-    .then(function (response) {
-      const characters = response.characters;
-      const characterPromises = [];
-
-      for (let i = 0; i < characters.length; ++i) {
-        characterPromises.push(getDataFrom(characters[i]));
-      }
-
-      Promise.all(characterPromises)
-        .then((results) => {
-          for (let i = 0; i < results.length; ++i) {
-            console.log(JSON.parse(results[i]).name);
-          }
-        })
-        .catch(errorHandler);
-    });
-}
-
-printMovieCharacters(process.argv[2]);
-
